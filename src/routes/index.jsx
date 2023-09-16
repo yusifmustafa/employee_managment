7c07ed13f411512cd5576f4b61b03b16486b63ac
Yusif <ymustafazade@std.beu.edu.az>
2023-09-16 22:24:29 +0400

    router page yaradildi

---
+++ src/routes/index.jsx
@@ -0,0 +1,14 @@
+import {createBrowserRouter} from "react-router-dom";
+
+createBrowserRouter(
+    [
+        {
+            path:"",
+            element:'',
+        },
+        {
+            path:'',
+            element:''
+        }
+    ]
+)
