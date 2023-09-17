6c759a9421dc0e75bfc01b60c469b81c452adadb
Yusif <ymustafazade@std.beu.edu.az>
2023-09-17 15:12:32 +0400

    global css yazildi,Not found sehifesi ucun css yazildi

--- src/pages/not_found/NotFound.jsx
+++ src/pages/not_found/NotFound.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React from "react";
 import "./not_found.css";
 
 const NotFound = () => {
-    return (
-        <>
-          <div className="header">
-              <h1>Server Error</h1>
-          </div>
-        </>
-    );
+  return (
+    <>
+      <div className="header">
+        <h1>Server Error</h1>
+      </div>
+      <div id="content">
+        <div className="content-container">
+          <fieldset>
+            <h2>404- File or directory not found.</h2>
+            <h3>
+              The resource you are looking for might have been removed, had its
+              name changed, or is temporarily unavailable.
+            </h3>
+          </fieldset>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default NotFound;
