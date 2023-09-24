732c19951416f4193a772ec99ceab3905268c18f
Yusif <ymustafazade@std.beu.edu.az>
2023-09-24 23:06:28 +0400

    Login sehifesi yaradildi

--- src/pages/login_page/LoginPage.jsx
+++ src/pages/login_page/LoginPage.jsx
@@ -1,9 +1,66 @@
 import React from "react";
+import {
+  Box,
+  Button,
+  Container,
+  createTheme,
+  CssBaseline,
+  TextField,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
 const LoginPage = () => {
+  const defaultTheme = createTheme();
   return (
     <>
-      Login
-     </>
+      <ThemeProvider theme={defaultTheme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 20,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography component="h1" variant="h5">
+              Login{" "}
+            </Typography>
+            <Box component="form" noValidate sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Login
+              </Button>
+            </Box>
+          </Box>
+        </Container>
+      </ThemeProvider>
+    </>
   );
 };
 
